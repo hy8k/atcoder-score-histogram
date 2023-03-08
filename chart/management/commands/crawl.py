6e9351adb633fbd0bd97855cc0f2b91cbed02808
sk461 <shiraken7101@gmail.com>
2023-03-09 00:42:59 +0900

    update

--- chart/management/commands/crawl.py
+++ chart/management/commands/crawl.py
@@ -17,9 +17,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open(BASE_DIR / 'ahc.json', 'r') as f:
             ahc_list = json.load(f)
+            contests = ahc_list['contests']
 
             # AHCの自動更新
-            for contest in ahc_list:
+            for contest in contests:
                 if contest['auto_update'] == True or options['all']:
                     contest_screen_name = contest['contest_screen_name']
                     standing_json = info.get_standings_json(
@@ -28,7 +29,7 @@ class Command(BaseCommand):
                         contest_screen_name, None, standing_json)
 
             # 次のAHCがあれば取得する
-            latest_contest_screen_name = ahc_list[-1]['contest_screen_name']
+            latest_contest_screen_name = contests[-1]['contest_screen_name']
             next_contest_screen_name = 'ahc' + \
                 str(int(latest_contest_screen_name[3:6])+1).zfill(3)
             next_contest_standings_json = info.get_standings_json(
@@ -40,7 +41,7 @@ class Command(BaseCommand):
                 info.update_chart_info(
                     next_contest_screen_name, None, next_contest_standings_json)
 
-                ahc_list.append({
+                contests.append({
                     'contest_screen_name': next_contest_screen_name,
                     'auto_update': True
                 })
@@ -50,12 +51,17 @@ class Command(BaseCommand):
                 next_contest_standings_json = info.get_standings_json(
                     next_contest_screen_name)
 
-        number_of_contests = len(ahc_list)
-        for i in range(0, number_of_contests-3):
-            ahc_list[i]['auto_update'] = False
-        for i in range(number_of_contests-1, max(0, number_of_contests-4), -1):
-            ahc_list[i]['auto_update'] = True
+        # 次回の自動更新に係る処理
+        # auto: 最新3件以外は自動更新しない
+        # semi-auto: 最新3件以外の自動更新を許容する（auto_updateを手動で設定可能）
+        if ahc_list['update_mode'] == 'auto':
+            number_of_contests = len(contests)
+            for i in range(0, number_of_contests-3):
+                contests[i]['auto_update'] = False
+            for i in range(number_of_contests-1, max(0, number_of_contests-4), -1):
+                contests[i]['auto_update'] = True
 
+        ahc_list['contests'] = contests
         with open(BASE_DIR / 'ahc.json', 'w') as f:
             json.dump(ahc_list, f)
 
@@ -64,13 +70,17 @@ class Command(BaseCommand):
             other_contests_list = json.load(f)
 
             for contest in other_contests_list:
+                contest_screen_name = contest['contest_screen_name']
+                standings_json = info.get_standings_json(contest_screen_name)
+
                 if contest['auto_update_all'] == True:
-                    contest_screen_name = contest['contest_screen_name']
-                    standing_json = info.get_standings_json(
-                        contest_screen_name)
                     info.update_chart_info(
                         contest_screen_name, None, standing_json)
+                elif contest['tasks'] != []:
+                    task_list = set()
+                    for task in contest['tasks']:
+                        if task['auto_update'] == True:
+                            task_list.add(task['screen_name'])
 
-            # タスクごとに更新（todo）
-
-        return
+                    info.update_chart_info(
+                        contest_screen_name, task_list, standings_json)
